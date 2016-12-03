713833f535847f9ac0682cb1485d537eb69f994c
Mathilde Arconte <marconte@xprdev.fr>
2016-12-03 17:54:54 +0100

    Archive-agenda sections

--- archive-agenda.php
+++ archive-agenda.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 	<main id="primary" class="content-area" role="main">
 
-		<div id="posts" class="posts posts-agenda">
+		<div class="posts posts-agenda">
 
 			<?php if ( have_posts() ) : ?>
 
@@ -20,55 +20,91 @@ get_header(); ?>
 						echo '<h1 class="page-title">Agenda</h1>' ;
 						the_archive_description( '<div class="taxonomy-description">', '</div>' );
 					?>
-				</header><!-- .page-header -->
+				</header>
+				<!-- .page-header -->
+				<div class="posts posts-agenda next-concerts">
+				<div class="header-posts">
+					<span class="banner">Nos prochains concerts</span>
+					<p><?php dynamic_sidebar( 'agenda-description' ); ?> </p>
+				</div>
 
 
-				<?php /* Start the Loop */ ?>
-				<?php
-									$today = date('Ymd');
-$args = array(
-    'post_type' => 'agenda',
-    'meta_key' => 'date',
-    'meta_query' => array(
-        array(
-            'key' => 'date'
-        ),
-        array(
-            'key' => 'date',
-            'value' => $today,
-            'compare' => '>='
-        )
-    ),
-    'orderby' => 'meta_value_num',
-    'order' => 'ASC'
-);
+					<?php	$today = date('Ymd');
+							$args = array(
+    								'post_type' => 'agenda',
+    								'meta_key' => 'date',
+    								'meta_query' =>
+										array(
+											array(
+            								'key' => 'date'
+        									),
+        									array(
+            								'key' => 'date',
+            								'value' => $today,
+            								'compare' => '>='
+											)
+    									),
+    								'orderby' => 'meta_value_num',
+    								'order' => 'ASC'
+									);
 
 						$loop = new WP_Query($args);?>
-				<?php while ($loop->have_posts() ) : $loop->the_post(); ?>
+						<?php while ($loop->have_posts() ) : $loop->the_post(); ?>
+						<?php	get_template_part( 'template-parts/content', 'agenda' ); ?>
+						<?php endwhile; ?>
+				</div>
+
+				<div class="posts posts-agenda previous-concerts">
+					<div class="header-posts">
+						<span class="banner">Concerts précédents</span>
+						<?php $count_posts = wp_count_posts('agenda'); ?>
+						<ul class="stats-agenda">
+									<li class="stats-item">
+									<span class="genericon genericon-day"></span>
+									<p>Concerts vus : <span class="counter"><?php echo $count_posts->publish ; ?></span></p>
+									</li>
+									<li class="stats-item">
+									<span class="genericon genericon-user"></span>
+									<p>Artistes vus : <span class="counter"><?php get_artists(); ?></span></p>
+									</li>
+									<li class="stats-item">
+									<span class="genericon genericon-star"></span>
+									<p>Grosse groupie : <?php get_most_viewed_artist(); ?> (<span class="counter"><?php get_most_viewed_artist_count(); ?></span>)</p>
+									</li>
+								</ul>
+					</div>
+
 
 					<?php
+							$args2 = array(
+    								'post_type' => 'agenda',
+    								'meta_key' => 'date',
+									'posts_per_page' => 1000,
+    								'meta_query' =>
+										array(
+											array(
+            								'key' => 'date'
+        									),
+        									array(
+            								'key' => 'date',
+											'value' => $today,
+            								'compare' => '<'
+											)
+    									),
+    								'orderby' => 'meta_value_num',
+    								'order' => 'DESC'
+									);
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						 get_template_part( 'template-parts/content', 'agenda' );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php the_posts_navigation(); ?>
-
-			<?php else : ?>
-
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-			<?php endif; ?>
-
-		</div><!-- .posts -->
-
-	</main><!-- #main -->
-
-
-<?php get_footer(); ?>
+						$loop2 = new WP_Query($args2);?>
+						<?php while ($loop2->have_posts() ) : $loop2->the_post(); ?>
+						<?php	get_template_part( 'template-parts/content', 'agenda' ); ?>
+						<?php endwhile; ?>
+				</div>
+									<?php else : ?>
+							<?php get_template_part( 'template-parts/content', 'none' ); ?>
+					<?php endif; ?>
+		</div>
+		<!-- .posts -->
+	</main>
+	<!-- #main -->
+	<?php get_footer(); ?>
